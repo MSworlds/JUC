fa38bc01d702c1c3b73a41fbce97487b8af2173e
shuaimeng <shuaimeng@hengtiansoft.com>
2019-06-11 17:37:33 +0800

    maven atguigu.juc my.juc

--- src/com/meng/juc/TestVolatile.java
+++ src/com/meng/juc/TestVolatile.java
@@ -1,7 +1,43 @@
 package com.meng.juc;
 
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Administrator on 2018-06-06.
  */
 public class TestVolatile {
+    public /*volatile*/ boolean  flag = false;
+
+    public void change() {
+        System.out.println("改变");
+        flag = true;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TestVolatile testVolatile = new TestVolatile();
+
+        new Thread(() -> {
+            while (true) {
+                if (testVolatile.flag) {
+                    System.out.println("==================================");
+                    break;
+                }
+            }
+
+        }).start();
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            testVolatile.change();
+
+
+        }).start();
+
+
+        System.out.println("over");
+    }
 }
--- src/com/meng/juc/Ticket.java
+++ src/com/meng/juc/Ticket.java
@@ -4,7 +4,7 @@ package com.meng.juc;
  * Created by Administrator on 2018-06-08.
  * 经典抢票demo
  */
- class Ticket implements Runnable {
+class Ticket implements Runnable {
 
     private int num = 0; // 出票数
     private int count = 10; // 剩余票数
@@ -15,7 +15,7 @@ package com.meng.juc;
     public void run() {
 
         while (true) {
-
+//            synchronized (this) {
             // 没有余票时，跳出循环
             if (count <= 0) {
                 break;
@@ -31,12 +31,11 @@ package com.meng.juc;
             }
             System.out.println("显示出票信息：" + Thread.currentThread().getName()
                     + "抢到第" + num + "张票，剩余" + count + "张票");
-
         }
     }
-
 }
 
+//}
 
 
 class TicketTest {
@@ -46,7 +45,7 @@ class TicketTest {
      */
     public static void main(String[] args) {
 
-        Ticket ticket=new Ticket();
+        Ticket ticket = new Ticket();
         // 　实例化几个抢票用户
         Thread mary = new Thread(ticket, "玛丽");
 
